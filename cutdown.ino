8957d2566635e86cd990120224137c21774f4b12
Sean Widmier <seanwidmier@gmail.com>
2019-10-25 15:06:43 -0500

    fix averaging

--- cutdown.ino
+++ cutdown.ino
@@ -8,11 +8,12 @@ void cutdown();
 
 Adafruit_BMP3XX bmp;
 uint32_t alt[10];
-uint32_t current_average, init_pressure;
-uint32_t counter = 0;
+uint32_t init_pressure;
+uint8_t counter = 0;
 bool cut=0;
 
 void setup() {
+  uint8_t x = 0;
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Master Cutdown -- Two Month 2019");
@@ -21,10 +22,10 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(HOTWIRE_PIN, OUTPUT);
 
+  while(!bmp.begin() && x++ < 10);
+  if(x == 10) while(1); // don't turn on anything if we can't boot the bmp
+
   // setup the LED at 50% duty cycle
-  if(bmp.begin()){
-    analogWrite(LED_PIN, 255/50);
-  }
 
   // take 50 readings over half a second and use the average as inital pressure
   init_pressure = 0;
@@ -34,22 +35,31 @@ void setup() {
     delay(10);
   }
   init_pressure /= 50;
+
+  // fill up buffer with readings so we can use that in averaging
+  for(int i = 0; i < 10; i++) {
+    bmp.performReading();
+    alt[i] = bmp.readAltitude(init_pressure);
+    delay(10);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint32_t average = 0;
   bmp.performReading();
-
   alt[counter%10] = bmp.readAltitude(init_pressure);
-  for(int i=0; i<counter; i++){
-    current_average += alt[i];
-  }
-  current_average /= 10;
 
-  if(current_average - initial_altitude >= 600 && !cut){
+  for(int i=0; i<10; i++) {
+    average += alt[i];
+  }
+  average /= 10;
+
+  if(average >= 600 && !cut){
     cutdown();
   }
   counter++;
+
+  delay(10);
 }
 
 void cutdown(){
